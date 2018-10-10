f0c0832116137c08da2b10b7ebb3a29413f6208f
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-10-10 14:19:07 +0600

    bootstrap table added

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -3,6 +3,8 @@ import { Alert,Glyphicon,Button,Modal } from 'react-bootstrap';
 import { Link } from 'react-router';
 import TodoEditForm from './TodoEditForm';
 import { BootstrapTable , TableHeaderColumn} from 'react-bootstrap-table';
+
+
 export default class Todos extends React.Component {
   constructor(props){
     super(props);
@@ -44,10 +46,47 @@ showDeleteModal(todoToDelete){
 cofirmDeleteTodo(){
 this.props.mappedDeleteTodo(this.props.mappedTodoState.todoToDelete);
 }
+
+renderShowsTotal(start, to, total) {
+  return (
+    <p style={ { color: 'blue' } }>
+      From { start } to { to }, totals is { total }&nbsp;&nbsp;(its a customize text)
+    </p>
+  );
+}
+
 render(){
+
+
     const todoState = this.props.mappedTodoState;
     const todos = todoState.todos;
     const editTodo = todoState.todoToEdit;
+
+
+    const options = {
+      page: 2,  // which page you want to show as default
+      sizePerPageList: [ {
+        text: '5', value: 5
+      }, {
+        text: '10', value: 10
+      }, {
+        text: 'All', value: todos.length
+      } ], // you can change the dropdown list for size per page
+      sizePerPage: 5,  // which size per page you want to locate as default
+      pageStartIndex: 0, // where to start counting the pages
+      paginationSize: 3,  // the pagination bar size.
+      prePage: 'Prev', // Previous page button text
+      nextPage: 'Next', // Next page button text
+      firstPage: 'First', // First page button text
+      lastPage: 'Last', // Last page button text
+      paginationShowsTotal: this.renderShowsTotal,  // Accept bool or function
+      paginationPosition: 'top'  // default is bottom, top and both is all available
+      // hideSizePerPage: true > You can hide the dropdown for sizePerPage
+      // alwaysShowAllBtns: true // Always show next and previous button
+      // withFirstAndLast: false > Hide the going to First and Last page button
+    };
+
+
     return(
       <div className="col-md-12">
       <h3 className="centerAlign">Works</h3>
@@ -72,9 +111,12 @@ render(){
       }
       </tbody> */}
 
-      <BootstrapTable data={ todos }>
-      <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
-      <TableHeaderColumn dataField='todoText'>Product Name</TableHeaderColumn>
+      <BootstrapTable data={ todos } pagination={ true } options={ options } >
+      <TableHeaderColumn dataField='id' isKey hidden></TableHeaderColumn>
+      <TableHeaderColumn dataField='todoText'>Work</TableHeaderColumn>
+      <TableHeaderColumn dataField='todoText'>Edit</TableHeaderColumn>
+      <TableHeaderColumn dataField='todoText'>Delete</TableHeaderColumn>
+      <TableHeaderColumn dataField='todoText'>View</TableHeaderColumn>
       </BootstrapTable>
 
       </table>
